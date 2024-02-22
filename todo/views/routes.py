a9dc4617505e5c58867f726f20aefc0620fedd5c
Lorenzo Policar <lorenzo_policar@yahoo.com>
2024-02-22 19:18:00 +1000

    set up flask api

---
+++ todo/views/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+
+
+api = Blueprint('api', __name__, url_prefix='/api/v1')
+
+@api.route('/health')
+def health():
+    return jsonify({"status": "ok"})
+
+@api.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify([])
+
+    
